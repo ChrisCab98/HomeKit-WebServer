6a4f0cb5dd1d6f8b2e7d418c9420c70aa43d56ff
Christopher CABANTOUS <chris@MacBook-Air-de-Christopher-2.local>
2020-01-23 00:19:17 +0100

    test button

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 app.config['SECRET'] = 'my secret key'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = '192.168.68.113' # Devisubox
-# app.config['MQTT_BROKER_URL'] = '192.168.1.29' # Home
+# app.config['MQTT_BROKER_URL'] = '192.168.68.113' # Devisubox
+app.config['MQTT_BROKER_URL'] = '192.168.1.29' # Home
 # app.config['MQTT_BROKER_URL'] = '127.0.0.1' # Localhost
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = ''
