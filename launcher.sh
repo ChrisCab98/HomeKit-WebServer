2b61bc0fd2c99e9b233472cf9b67e9baa51fb536
christophercabantous <christophercabantous@gmail.com>
2020-01-30 19:17:20 +0000

    Execute git pull as Pi user

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 cd /home/pi/HomeKit-WebServer
 echo "Update Homekit-WebServer from GitHub"
-git pull
+su -pi -c "git pull"
 echo "Start HomeKit-WebServer"
 su - pi -c "python3 /home/pi/HomeKit-WebServer/app.py" &
