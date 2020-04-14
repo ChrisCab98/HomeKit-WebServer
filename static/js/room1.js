09cb47cc95dbb0eed2e790dec3c5a1e5c16680ee
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-14 22:32:15 +0200

    fix misalignment of Name / Status of devices

--- static/js/room1.js
+++ static/js/room1.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     var socket = io.connect('http://' + document.domain + ':' + location.port);
 
     console.log('Page has been refreshed')
-    // $('#deskLampImgGlow').hide();
 
     var topic = 'cmnd/smartSurgeOutlet3/power';
     var message = '';
@@ -19,6 +18,11 @@ $(document).ready(function () {
     var qos3 = 0;
     var data3 = '{"topic": "' + topic3 + '", "message": "' + message3 + '", "qos": ' + qos3 + '}';
 
+    var topic4 = 'cmnd/smartSurgeOutlet1/power';
+    var message4 = '';
+    var qos4 = 0;
+    var data4 = '{"topic": "' + topic4 + '", "message": "' + message4 + '", "qos": ' + qos4 + '}';
+
     socket.emit('publish', data = data);
     console.log(data)
 
@@ -28,35 +32,10 @@ $(document).ready(function () {
     socket.emit('publish', data3 = data3);
     console.log(data3)
 
-    window.onload = function () {
-
-        // var topic = 'cmnd/smartSurgeOutlet3/power';
-        // var message = '';
-        // var qos = 0;
-        // var data = '{"topic": "' + topic + '", "message": "' + message + '", "qos": ' + qos + '}';
-
-        // var topic2 = 'cmnd/smartSurgeOutlet4/power';
-        // var message2 = '';
-        // var qos2 = 0;
-        // var data2 = '{"topic": "' + topic2 + '", "message": "' + message2 + '", "qos": ' + qos2 + '}';
-
-        // var topic3 = 'cmnd/smartSurgeOutlet2/power';
-        // var message3 = '';
-        // var qos3 = 0;
-        // var data3 = '{"topic": "' + topic3 + '", "message": "' + message3 + '", "qos": ' + qos3 + '}';
-
-        // socket.emit('publish', data = data);
-        // console.log(data)
-
-        // socket.emit('publish', data2 = data2);
-        // console.log(data2)
-
-        // socket.emit('publish', data3 = data3);
-        // console.log(data3)
-    };
+    socket.emit('publish', data4 = data4);
+    console.log(data4)
 
     $(".dropdown-toggle").dropdown();
-
 
     $('#deskLamp').click(function (event) {
         var topic = 'cmnd/smartSurgeOutlet3/power';
@@ -99,6 +78,24 @@ $(document).ready(function () {
         var message = '';
         var qos = 0;
         var state = $('#screenMonitorState').text();
+
+        if (state == 'Allumé') {
+            message = 'off';
+        }
+
+        else {
+            message = 'on';
+        }
+
+        var data = '{"topic": "' + topic + '", "message": "' + message + '", "qos": ' + qos + '}';
+        socket.emit('publish', data = data);
+    });
+
+    $('#solderingIron').click(function (event) {
+        var topic = 'cmnd/smartSurgeOutlet1/power';
+        var message = '';
+        var qos = 0;
+        var state = $('#solderingIronState').text();
 
         if (state == 'Allumé') {
             message = 'off';
@@ -197,6 +194,35 @@ $(document).ready(function () {
                 $('#screenMonitorState').addClass("accessorieStatusOff")
                 $('#screenMonitorName').removeClass("accessorieNameOn")
                 $('#screenMonitorName').addClass("accessorieNameOff")
+            }
+            img.setAttribute("src", imgAttribute);
+
+        }
+
+        if (data['topic'] == 'stat/smartSurgeOutlet1/POWER') {
+            var img = document.getElementById("solderingIronImg");
+            var imgAttribute = img.getAttribute("src");
+            if (data['payload'] == 'ON') {
+                $('#solderingIronState').text('Allumé');
+                imgAttribute = "../static/png/powerOutlet.png"
+
+                $('#solderingIron').removeClass("accessorie")
+                $('#solderingIron').addClass("accessorieActive")
+                $('#solderingIronState').removeClass("accessorieStatusOff")
+                $('#solderingIronState').addClass("accessorieStatusOn")
+                $('#solderingIronName').removeClass("accessorieNameOff")
+                $('#solderingIronName').addClass("accessorieNameOn")
+            }
+            else {
+                $('#solderingIronState').text('Éteint');
+                imgAttribute = "../static/png/powerOutletOff.png"
+
+                $('#solderingIron').removeClass("accessorieActive")
+                $('#solderingIron').addClass("accessorie")
+                $('#solderingIronState').removeClass("accessorieStatusOn")
+                $('#solderingIronState').addClass("accessorieStatusOff")
+                $('#solderingIronName').removeClass("accessorieNameOn")
+                $('#solderingIronName').addClass("accessorieNameOff")
             }
             img.setAttribute("src", imgAttribute);
 
